0ae6b7486e3194cfecefd47cbc657aa8db07b44a
Iverson Diles <ivcotad@gmail.com>
2023-07-05 22:03:16 -0700

    cool wraps

--- lua/ivcota/init.lua
+++ lua/ivcota/init.lua
@@ -4,50 +4,49 @@ require("ivcota.settings")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-
-
 local plugins = {
-	'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	'nvim-lua/plenary.nvim',
-	'nvim-treesitter/nvim-treesitter',
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.2",
+	"nvim-lua/plenary.nvim",
+	"nvim-treesitter/nvim-treesitter",
 	build = ":masonupdate", -- :masonupdate updates registry contents
-	{ 'rose-pine/neovim', name = 'rose-pine' },
-	{'folke/tokyonight.nvim'},
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "folke/tokyonight.nvim" },
 	{
-		'vonheikemen/lsp-zero.nvim',
-		branch = 'v2.x',
+		"vonheikemen/lsp-zero.nvim",
+		branch = "v2.x",
 		dependencies = {
 			-- lsp support
-			{'neovim/nvim-lspconfig'},             -- required
-			{                                      -- optional
-				'williamboman/mason.nvim',
+			{ "neovim/nvim-lspconfig" }, -- required
+			{ -- optional
+				"williamboman/mason.nvim",
 				build = function()
-					pcall(vim.cmd, 'masonupdate')
+					pcall(vim.cmd, "masonupdate")
 				end,
 			},
-			{'williamboman/mason-lspconfig.nvim'}, -- optional
+			{ "williamboman/mason-lspconfig.nvim" }, -- optional
 
 			-- autocompletion
-			{'hrsh7th/nvim-cmp'},     -- required
-			{'hrsh7th/cmp-nvim-lsp'}, -- required
-			{'l3mon4d3/luasnip'},     -- required
-		}
+			{ "hrsh7th/nvim-cmp" }, -- required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- required
+			{ "l3mon4d3/luasnip" }, -- required
+		},
 	},
-	'kdheepak/lazygit.nvim',
-	'theprimeagen/harpoon',
-	'github/copilot.vim',
+	"kdheepak/lazygit.nvim",
+	"theprimeagen/harpoon",
+	"github/copilot.vim",
 	{
 		"folke/which-key.nvim",
 		event = "VeryLazy",
@@ -55,17 +54,16 @@ local plugins = {
 			vim.o.timeout = true
 			vim.o.timeoutlen = 300
 		end,
-		opts = {}
+		opts = {},
 	},
-	{'akinsho/toggleterm.nvim', version = "*", config = true},
-	'jose-elias-alvarez/null-ls.nvim',
+	{ "akinsho/toggleterm.nvim", version = "*", config = true },
+	"jose-elias-alvarez/null-ls.nvim",
+	"tpope/vim-surround",
 }
 
 local opts = {}
 
-
 require("lazy").setup(plugins, opts)
 
-
-vim.cmd('colorscheme rose-pine')
+vim.cmd("colorscheme rose-pine")
 print("hello world")
