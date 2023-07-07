c761e3de8c50226134267d72d8f9725200a0382d
Iverson Diles <ivcotad@gmail.com>
2023-07-06 20:42:52 -0700

    tmux keymaps

--- after/plugin/tmux.lua
+++ after/plugin/tmux.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { silent = true, noremap = true })
-vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { silent = true, noremap = true })
-vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { silent = true, noremap = true })
-vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { silent = true, noremap = true })
+vim.keymap.set("n", "<C-S-h>", "<cmd>TmuxResizeLeft<CR>", { silent = true, noremap = true })
+vim.keymap.set("n", "<C-S-l>", "<cmd>TmuxResizeRight<CR>", { silent = true, noremap = true })
+vim.keymap.set("n", "<C-S-j>", "<cmd>TmuxResizeDown<CR>", { silent = true, noremap = true })
+vim.keymap.set("n", "<C-S-k>", "<cmd>TmuxResizeUp<CR>", { silent = true, noremap = true })
