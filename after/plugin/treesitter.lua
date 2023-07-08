5df3c4545b54fb23dbf782547a7cf38e34f80c4a
Iverson Diles <ivcotad@gmail.com>
2023-07-07 22:36:45 -0700

    auto treesitter install

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,13 @@
-require'nvim-treesitter.configs'.setup {
-  indent = {
-    enable = true
-  }
-}
+require("nvim-treesitter.configs").setup({
+	indent = {
+		enable = true,
+	},
 
+	auto_install = true,
+
+	ensure_installed = { "c", "lua", "vim", "typescript", "javascript", "rust" },
+
+	highlight = {
+		enable = true,
+	},
+})
