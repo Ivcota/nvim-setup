c86fa4b9ade7f5f44dbbfb94c634eb9cc4d732f9
Iverson Diles <ivcotad@gmail.com>
2023-07-19 10:56:05 -0700

    zenmode

--- lua/ivcota/init.lua
+++ lua/ivcota/init.lua
@@ -4,84 +4,85 @@ require("ivcota.settings")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.2",
-  "nvim-lua/plenary.nvim",
-  "nvim-treesitter/nvim-treesitter",
-  build = ":masonupdate", -- :masonupdate updates registry contents
-  { "rose-pine/neovim",     name = "rose-pine" },
-  { "folke/tokyonight.nvim" },
-  {
-    "vonheikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    dependencies = {
-      -- lsp support
-      { "neovim/nvim-lspconfig" }, -- required
-      {
-        -- optional
-        "williamboman/mason.nvim",
-        build = function()
-          pcall(vim.cmd, "masonupdate")
-        end,
-      },
-      { "williamboman/mason-lspconfig.nvim" }, -- optional
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.2",
+	"nvim-lua/plenary.nvim",
+	"nvim-treesitter/nvim-treesitter",
+	build = ":masonupdate", -- :masonupdate updates registry contents
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "folke/tokyonight.nvim" },
+	{
+		"vonheikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- lsp support
+			{ "neovim/nvim-lspconfig" }, -- required
+			{
+				-- optional
+				"williamboman/mason.nvim",
+				build = function()
+					pcall(vim.cmd, "masonupdate")
+				end,
+			},
+			{ "williamboman/mason-lspconfig.nvim" }, -- optional
 
-      -- autocompletion
-      { "hrsh7th/nvim-cmp" },  -- required
-      { "hrsh7th/cmp-nvim-lsp" }, -- required
-      { "l3mon4d3/luasnip" },  -- required
-    },
-  },
-  "kdheepak/lazygit.nvim",
-  "theprimeagen/harpoon",
-  "github/copilot.vim",
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-    end,
-    opts = {},
-  },
-  { "akinsho/toggleterm.nvim", version = "*", config = true },
-  "jose-elias-alvarez/null-ls.nvim",
-  "tpope/vim-surround",
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  "vim-airline/vim-airline",
-  "vim-test/vim-test",
-  "HiPhish/nvim-ts-rainbow2",
-  "rstacruz/vim-closer",
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-  },
-  "windwp/nvim-ts-autotag",
-  -- help me identify which plugin is causing the issue
-  {
-    "j-hui/fidget.nvim",
-    version = "legacy",
-  },
-  "tpope/vim-fugitive",
-  -- "preservim/nerdtree",
+			-- autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- required
+			{ "l3mon4d3/luasnip" }, -- required
+		},
+	},
+	"kdheepak/lazygit.nvim",
+	"theprimeagen/harpoon",
+	"github/copilot.vim",
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {},
+	},
+	{ "akinsho/toggleterm.nvim", version = "*", config = true },
+	"jose-elias-alvarez/null-ls.nvim",
+	"tpope/vim-surround",
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	"vim-airline/vim-airline",
+	"vim-test/vim-test",
+	"HiPhish/nvim-ts-rainbow2",
+	"rstacruz/vim-closer",
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	},
+	"windwp/nvim-ts-autotag",
+	-- help me identify which plugin is causing the issue
+	{
+		"j-hui/fidget.nvim",
+		version = "legacy",
+	},
+	"tpope/vim-fugitive",
+	-- "preservim/nerdtree",
+	"folke/zen-mode.nvim",
 }
 
 local opts = {}
