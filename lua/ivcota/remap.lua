734be90c9f1eef1db3cebaaa888928809316930e
Iverson Diles <ivcotad@gmail.com>
2023-07-25 22:09:02 -0700

    update keymaps

--- lua/ivcota/remap.lua
+++ lua/ivcota/remap.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- vs code like remap
 vim.keymap.set("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
 
@@ -14,3 +17,4 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
