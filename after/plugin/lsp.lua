3dd31dee1b86eb1d9fed690f05dfd676f1bcb2b6
Iverson Diles <ivcotad@gmail.com>
2023-08-05 17:57:19 -0700

    reconfig lsp setup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,59 +1,72 @@
+-- Import required modules
 local lsp = require("lsp-zero")
+local cmp = require('cmp')
+local lspconfig = require('lspconfig')
 
+-- LSP setup
 lsp.preset("recommended")
-
-lsp.ensure_installed({
-  'tsserver',
-  'rust_analyzer',
-})
-
--- Fix Undefined global 'vim'
+lsp.ensure_installed({ 'tsserver', 'rust_analyzer' })
 lsp.nvim_workspace()
 
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+-- CMP setup
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
 })
-
 cmp_mappings['<Tab>'] = cmp.mapping.confirm({ select = true })
 cmp_mappings['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select)
+lsp.setup_nvim_cmp({ mapping = cmp_mappings })
 
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
-})
-
+-- LSP preferences
 lsp.set_preferences({
     suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+    sign_icons = { error = 'E', warn = 'W', hint = 'H', info = 'I' }
 })
+
+-- LSP key mappings
+local function set_lsp_keymap(mode, key, action)
+    local opts = { buffer = 0, remap = false }  -- Adjust buffer value if needed
+    vim.keymap.set(mode, key, action, opts)
+end
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    set_lsp_keymap("n", "gd", function() vim.lsp.buf.definition() end)
+    set_lsp_keymap("n", "gh", function() vim.lsp.buf.hover() end)
+    set_lsp_keymap("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+    set_lsp_keymap("n", "<leader>vd", function() vim.diagnostic.open_float() end)
+    set_lsp_keymap("n", "[d", function() vim.diagnostic.goto_next() end)
+    set_lsp_keymap("n", "]d", function() vim.diagnostic.goto_prev() end)
+    set_lsp_keymap("n", "<leader>vca", function() vim.lsp.buf.code_action() end)
+    set_lsp_keymap("n", "<leader>gr", function() vim.lsp.buf.references() end)
+    set_lsp_keymap("n", "<leader>vrn", function() vim.lsp.buf.rename() end)
+    set_lsp_keymap("i", "<C-h>", function() vim.lsp.buf.signature_help() end)
 end)
 
-lsp.setup()
+-- Custom function to organize imports
+local function organize_imports()
+    local params = {
+        command = "_typescript.organizeImports",
+        arguments = {vim.api.nvim_buf_get_name(0)},
+        title = ""
+    }
+    vim.lsp.buf.execute_command(params)
+end
 
-vim.diagnostic.config({
-    virtual_text = true
-})
+-- LSP configuration for tsserver
+lspconfig.tsserver.setup {
+    on_attach = lsp.on_attach,
+    capabilities = lsp.capabilities,
+    commands = {
+        OrganizeImports = {
+            organize_imports,
+            description = "Organize Imports"
+        }
+    }
+}
+
+-- Final setup
+lsp.setup()
+vim.diagnostic.config({ virtual_text = true })
