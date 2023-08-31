863d64f099e3d2d3afb70bfb2bcc8bfc29c95756
Iverson Diles <ivcotad@gmail.com>
2023-08-30 22:43:45 -0700

    python format

--- after/plugin/nullrs.lua
+++ after/plugin/nullrs.lua
@@ -5,6 +5,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.completion.spell,
 		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.black,
 		-- null_ls.builtins.diagnostics.eslint,
 	},
 })
