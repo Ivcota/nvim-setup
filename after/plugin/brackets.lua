c7d4a6f94b5d6e9c37185dba3711ce2b93a2c96d
Iverson Diles <ivcotad@gmail.com>
2023-10-27 16:26:49 -1000

    remove broken plugin - brackets

--- after/plugin/brackets.lua
+++ after/plugin/brackets.lua
@@ -1,9 +1,1 @@
-require("nvim-treesitter.configs").setup({
-	rainbow = {
-		enable = true,
-		-- Which query to use for finding delimiters
-		query = "rainbow-parens",
-		-- Highlight the entire buffer all at once
-		strategy = require("ts-rainbow").strategy.global,
-	},
-})
+
