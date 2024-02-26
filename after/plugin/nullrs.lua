9b1b87475ea40db7f699c3f5ffecf49c166a5691
Iverson Diles <iverson.diles@mass.gov>
2024-02-26 09:41:30 -0700

    lua formatting

--- after/plugin/nullrs.lua
+++ after/plugin/nullrs.lua
@@ -1,19 +1,18 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.completion.spell,
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.black,
-    null_ls.builtins.formatting.sqlfmt,
-    -- null_ls.builtins.diagnostics.eslint,
-  },
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.completion.spell,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.sqlfmt,
+	},
 })
 
 vim.keymap.set(
-  "n",
-  "<leader>fm",
-  "<cmd>lua vim.lsp.buf.format({ timeout_ms = 8000 })<CR>",
-  { noremap = true, silent = true }
+	"n",
+	"<leader>fm",
+	"<cmd>lua vim.lsp.buf.format({ timeout_ms = 8000 })<CR>",
+	{ noremap = true, silent = true }
 )
