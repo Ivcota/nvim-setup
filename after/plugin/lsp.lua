fdc2af00a4af0a30c31f7c501a98664ce1159ad5
Iverson <ivcotad@gmail.com>
2024-03-17 18:59:13 -0700

    lsp file formatted

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,32 +1,35 @@
 -- Import required modules
 local lsp = require("lsp-zero")
-local cmp = require('cmp')
-local lspconfig = require('lspconfig')
+local cmp = require("cmp")
+local lspconfig = require("lspconfig")
 
 -- LSP setup
 lsp.preset("recommended")
-lsp.ensure_installed({ 'tsserver', 'rust_analyzer' })
+lsp.ensure_installed({ "tsserver", "rust_analyzer" })
 lsp.nvim_workspace()
 
 -- CMP setup
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+  [[
   autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
-]], true)
+]],
+  true
+)
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+  ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+  ["<C-y>"] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
-cmp_mappings['<Tab>'] = cmp.mapping.confirm({ select = true })
-cmp_mappings['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select)
+cmp_mappings["<Tab>"] = cmp.mapping.confirm({ select = true })
+cmp_mappings["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select)
 lsp.setup_nvim_cmp({ mapping = cmp_mappings })
 
 -- LSP preferences
 lsp.set_preferences({
   suggest_lsp_servers = false,
-  sign_icons = { error = 'E', warn = 'W', hint = 'H', info = 'I' }
+  sign_icons = { error = "E", warn = "W", hint = "H", info = "I" },
 })
 
 -- LSP key mappings
@@ -36,16 +39,36 @@ local function set_lsp_keymap(mode, key, action)
 end
 
 lsp.on_attach(function(client, bufnr)
-  set_lsp_keymap("n", "gd", function() vim.lsp.buf.definition() end)
-  set_lsp_keymap("n", "gh", function() vim.lsp.buf.hover() end)
-  set_lsp_keymap("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
-  set_lsp_keymap("n", "<leader>vd", function() vim.diagnostic.open_float() end)
-  set_lsp_keymap("n", "[d", function() vim.diagnostic.goto_next() end)
-  set_lsp_keymap("n", "]d", function() vim.diagnostic.goto_prev() end)
-  set_lsp_keymap("n", "<leader>vca", function() vim.lsp.buf.code_action() end)
-  set_lsp_keymap("n", "<leader>gr", function() vim.lsp.buf.references() end)
-  set_lsp_keymap("n", "<leader>vrn", function() vim.lsp.buf.rename() end)
-  set_lsp_keymap("i", "<C-h>", function() vim.lsp.buf.signature_help() end)
+  set_lsp_keymap("n", "gd", function()
+    vim.lsp.buf.definition()
+  end)
+  set_lsp_keymap("n", "gh", function()
+    vim.lsp.buf.hover()
+  end)
+  set_lsp_keymap("n", "<leader>vws", function()
+    vim.lsp.buf.workspace_symbol()
+  end)
+  set_lsp_keymap("n", "<leader>vd", function()
+    vim.diagnostic.open_float()
+  end)
+  set_lsp_keymap("n", "[d", function()
+    vim.diagnostic.goto_next()
+  end)
+  set_lsp_keymap("n", "]d", function()
+    vim.diagnostic.goto_prev()
+  end)
+  set_lsp_keymap("n", "<leader>vca", function()
+    vim.lsp.buf.code_action()
+  end)
+  set_lsp_keymap("n", "<leader>gr", function()
+    vim.lsp.buf.references()
+  end)
+  set_lsp_keymap("n", "<leader>vrn", function()
+    vim.lsp.buf.rename()
+  end)
+  set_lsp_keymap("i", "<C-h>", function()
+    vim.lsp.buf.signature_help()
+  end)
 end)
 
 -- Custom function to organize imports
@@ -53,27 +76,26 @@ local function organize_imports()
   local params = {
     command = "_typescript.organizeImports",
     arguments = { vim.api.nvim_buf_get_name(0) },
-    title = ""
+    title = "",
   }
   vim.lsp.buf.execute_command(params)
 end
 
 -- LSP configuration for tsserver
-lspconfig.tsserver.setup {
+lspconfig.tsserver.setup({
   on_attach = lsp.on_attach,
   capabilities = lsp.capabilities,
   commands = {
     OrganizeImports = {
       organize_imports,
-      description = "Organize Imports"
-    }
+      description = "Organize Imports",
+    },
   },
   preferences = {
     importModuleSpecifierPreference = "relative",
-    importModuleSpeciferEnding = "minimal"
-  }
-
-}
+    importModuleSpeciferEnding = "minimal",
+  },
+})
 
 -- Final setup
 lsp.setup()
