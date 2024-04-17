75e2860858def88f36068a941bcf3c6756df4bb9
Iverson Diles <ivcotad@gmail.com>
2024-04-17 07:30:01 -0700

    config: phat curser

--- lua/ivcota/settings.lua
+++ lua/ivcota/settings.lua
@@ -1,18 +1,21 @@
+vim.opt.guicursor = ""
+
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.backup = false
-vim.opt.expandtab = true
-vim.opt.smartindent = true
 
-vim.opt.scrolloff = 8
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
--- vim.opt.guicursor = ""
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
-vim.opt.cursorline = true
 
+vim.opt.scrolloff = 8
+vim.opt.guicursor = "a:blinkon100"
+vim.opt.backup = false
+vim.opt.cursorline = true
