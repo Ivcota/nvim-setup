b283c852a45b421eff4adb648aaa7d5f6c974101
Iverson Diles <ivcotad@gmail.com>
2024-10-11 10:10:51 -0700

    fixup! add supermaven

--- after/plugin/supermaven.lua
+++ after/plugin/supermaven.lua
@@ -1,17 +1,16 @@
 require("supermaven-nvim").setup({
   keymaps = {
-    accept_suggestion = "<Tab>",
+    accept_suggestion = "<C-m>",
     clear_suggestion = "<C-]>",
-    accept_word = "<C-j>",
   },
   ignore_filetypes = { cpp = true }, -- or { "cpp", }
   color = {
     suggestion_color = "#ffffff",
     cterm = 244,
   },
-  log_level = "info", -- set to "off" to disable logging completely
+  log_level = "info",                -- set to "off" to disable logging completely
   disable_inline_completion = false, -- disables inline completion for use with cmp
-  disable_keymaps = false, -- disables built in keymaps for more manual control
+  disable_keymaps = false,           -- disables built in keymaps for more manual control
   condition = function()
     return false
   end -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
