0246d90a2ac586e931d5b1f4436e62b0ac440b62
Iverson <ivcotad@gmail.com>
2025-08-02 17:09:53 -0700

    fix devicons

--- lua/ivcota/plugins.lua
+++ lua/ivcota/plugins.lua
@@ -16,6 +16,7 @@ local plugins = {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.2",
 	"nvim-lua/plenary.nvim",
+	"nvim-tree/nvim-web-devicons",
 	"nvim-treesitter/nvim-treesitter",
 	build = ":masonupdate", -- :masonupdate updates registry contents
 	{ "rose-pine/neovim", name = "rose-pine" },
