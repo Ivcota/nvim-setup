4f24939a40889e039e67246d230db134edd86436
Iverson <ivcotad@gmail.com>
2025-08-11 18:11:12 -0700

    replace copilot with supermaven

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,146 +10,144 @@ lsp.nvim_workspace()
 
 -- CMP setup
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "sql", "mysql", "plsql" },
-  callback = function()
-    require("cmp").setup.buffer({
-      sources = {
-        { name = "vim-dadbod-completion" },
-        { name = "copilot" }
-      },
-      formatting = {
-        format = require('lspkind').cmp_format({
-          mode = "symbol",
-          maxwidth = 50,
-          ellipsis_char = '...',
-          symbol_map = { Copilot = "", }
-        })
-      }
-    })
-  end,
+	pattern = { "sql", "mysql", "plsql" },
+	callback = function()
+		require("cmp").setup.buffer({
+			sources = {
+				{ name = "vim-dadbod-completion" },
+			},
+			formatting = {
+				format = require("lspkind").cmp_format({
+					mode = "symbol",
+					maxwidth = 50,
+					ellipsis_char = "...",
+				}),
+			},
+		})
+	end,
 })
 
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-  ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-  ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
 })
 cmp_mappings["<Tab>"] = cmp.mapping.confirm({ select = true })
 cmp_mappings["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select)
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings,
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "copilot" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "path" },
-  },
-  sorting = {
-    priority_weight = 2,
-    comparators = {
-      require("copilot_cmp.comparators").prioritize,
-      cmp.config.compare.offset,        -- Items closer to the cursor position
-      cmp.config.compare.exact,         -- Exact matches take precedence
-      cmp.config.compare.score,         -- Fuzzy matching score
-      cmp.config.compare.locality,      -- Items near the cursor in the document
-      cmp.config.compare.recently_used, -- Favor recently used completions
-      cmp.config.compare.kind,          -- Group items by their kind/type
-      cmp.config.compare.sort_text,     -- Sort using the provided sort_text
-      cmp.config.compare.length,        -- Prefer shorter completion entries
-      cmp.config.compare.order,         -- Fallback: maintain the original order
-    },
-  },
+	mapping = cmp_mappings,
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "supermaven" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+	},
+	sorting = {
+		priority_weight = 2,
+		comparators = {
+			cmp.config.compare.offset, -- Items closer to the cursor position
+			cmp.config.compare.exact, -- Exact matches take precedence
+			cmp.config.compare.score, -- Fuzzy matching score
+			cmp.config.compare.locality, -- Items near the cursor in the document
+			cmp.config.compare.recently_used, -- Favor recently used completions
+			cmp.config.compare.kind, -- Group items by their kind/type
+			cmp.config.compare.sort_text, -- Sort using the provided sort_text
+			cmp.config.compare.length, -- Prefer shorter completion entries
+			cmp.config.compare.order, -- Fallback: maintain the original order
+		},
+	},
 })
 -- LSP preferences
 lsp.set_preferences({
-  suggest_lsp_servers = false,
-  sign_icons = { error = "E", warn = "W", hint = "H", info = "I" },
+	suggest_lsp_servers = false,
+	sign_icons = { error = "E", warn = "W", hint = "H", info = "I" },
 })
 
 -- LSP key mappings
 local function set_lsp_keymap(mode, key, action)
-  local opts = { buffer = 0, remap = false } -- Adjust buffer value if needed
-  vim.keymap.set(mode, key, action, opts)
+	local opts = { buffer = 0, remap = false } -- Adjust buffer value if needed
+	vim.keymap.set(mode, key, action, opts)
 end
 
 lsp.on_attach(function(client, bufnr)
-  pcall(vim.api.nvim_buf_del_keymap, bufnr, "n", "gr")
-  pcall(vim.api.nvim_buf_del_keymap, bufnr, "n", "gh")
-  pcall(vim.api.nvim_buf_del_keymap, bufnr, "n", "gd")
-  -- set_lsp_keymap("n", "gd", function() vim.lsp.buf.definition() end)
-  set_lsp_keymap("n", "gh", function() vim.lsp.buf.hover() end)
-  -- set_lsp_keymap("n", "<leader>gr", function() vim.lsp.buf.references() end)
-  set_lsp_keymap("n", "<leader>vws", function()
-    vim.lsp.buf.workspace_symbol()
-  end)
-  set_lsp_keymap("n", "<leader>vd", function()
-    vim.diagnostic.open_float()
-  end)
-  set_lsp_keymap("n", "[d", function()
-    vim.diagnostic.goto_next()
-  end)
-  set_lsp_keymap("n", "]d", function()
-    vim.diagnostic.goto_prev()
-  end)
-  set_lsp_keymap("n", "<leader>vca", function()
-    vim.lsp.buf.code_action()
-  end)
-  set_lsp_keymap("n", "<leader>vrn", function()
-    vim.lsp.buf.rename()
-  end)
-  set_lsp_keymap("i", "<C-h>", function()
-    vim.lsp.buf.signature_help()
-  end)
+	pcall(vim.api.nvim_buf_del_keymap, bufnr, "n", "gr")
+	pcall(vim.api.nvim_buf_del_keymap, bufnr, "n", "gh")
+	pcall(vim.api.nvim_buf_del_keymap, bufnr, "n", "gd")
+	-- set_lsp_keymap("n", "gd", function() vim.lsp.buf.definition() end)
+	set_lsp_keymap("n", "gh", function()
+		vim.lsp.buf.hover()
+	end)
+	-- set_lsp_keymap("n", "<leader>gr", function() vim.lsp.buf.references() end)
+	set_lsp_keymap("n", "<leader>vws", function()
+		vim.lsp.buf.workspace_symbol()
+	end)
+	set_lsp_keymap("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end)
+	set_lsp_keymap("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end)
+	set_lsp_keymap("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end)
+	set_lsp_keymap("n", "<leader>vca", function()
+		vim.lsp.buf.code_action()
+	end)
+	set_lsp_keymap("n", "<leader>vrn", function()
+		vim.lsp.buf.rename()
+	end)
+	set_lsp_keymap("i", "<C-h>", function()
+		vim.lsp.buf.signature_help()
+	end)
 end)
-
 
 -- Custom function to organize imports
 local function organize_imports()
-  local params = {
-    command = "_typescript.organizeImports",
-    arguments = { vim.api.nvim_buf_get_name(0) },
-    title = "",
-  }
-  vim.lsp.buf.execute_command(params)
+	local params = {
+		command = "_typescript.organizeImports",
+		arguments = { vim.api.nvim_buf_get_name(0) },
+		title = "",
+	}
+	vim.lsp.buf.execute_command(params)
 end
 
 lspconfig.gopls.setup({})
 
 lspconfig.ts_ls.setup({
-  on_attach = lsp.on_attach,
-  capabilities = lsp.capabilities,
-  commands = {
-    OrganizeImports = {
-      organize_imports,
-      description = "Organize Imports",
-    },
-  },
-  preferences = {
-    importModuleSpecifierPreference = "relative",
-    importModuleSpeciferEnding = "minimal",
-  },
+	on_attach = lsp.on_attach,
+	capabilities = lsp.capabilities,
+	commands = {
+		OrganizeImports = {
+			organize_imports,
+			description = "Organize Imports",
+		},
+	},
+	preferences = {
+		importModuleSpecifierPreference = "relative",
+		importModuleSpeciferEnding = "minimal",
+	},
 })
 
 lspconfig.cssls.setup({
-  filetypes = { "css", "scss", "less", "vue", "svelte" },
+	filetypes = { "css", "scss", "less", "vue", "svelte" },
 })
 
 lspconfig.pyright.setup({
-  on_attach = lsp.on_attach,
-  capabilities = lsp.capabilities,
-  settings = {
-    python = {
-      analysis = {
-        autoSearchPaths = true,
-        diagnosticMode = "workspace",
-        useLibraryCodeForTypes = true,
-      },
-    },
-  },
+	on_attach = lsp.on_attach,
+	capabilities = lsp.capabilities,
+	settings = {
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				diagnosticMode = "workspace",
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
 })
 
 -- Final setup
